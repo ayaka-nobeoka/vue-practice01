557c2f9c7e8d5b0c5b852e2c1ed8e36c4a75569f
延岡あやか <nobeokaayaka@nobeokaayakanoMacBook-Air.local>
2025-07-07 10:48:14 +0900

    fromの設置まで

--- vue-practice08/src/App.vue
+++ vue-practice08/src/App.vue
@@ -1,16 +1,36 @@
 <script setup lang="ts">
 import { ref } from "vue";
+const name = ref<string>("");
+const number = ref<string>("");
+const mail = ref<string>("");
+const picked = ref<string>("");
+const textareaText = ref<string>("");
+const isChecked = ref<string>("");
+function handleSubmit(){
+
+}
+
 </script>
 <template>
-  <div>
-    <label for="name"
-      >お名前
-      <input type="text" id="name" v-model="name" />
-    </label>
-    <label for="number"
-      >電話番号
-      <input type="text" id="name" v-model="number" />
-    </label>
+  <div class="container">
+    <from @submit.prevent="handleSubmit">
+    <label for="name">お名前 </label>
+    <input type="text" id="name" v-model="name" />
+    <label for="number">電話番号 </label>
+    <input type="text" id="number" v-model="number" />
+    <label for="mail">メールアドレス</label>
+    <input type="text" id="mail" v-model="mail" />
+
+    <input type="radio" v-model="picked"  id="phone" value="電話"/>電話</input>
+    <input type="radio" v-model="picked" value="メール">メール</input>
+    <textarea v-model="textareaText" placeholder="お問い合わせ内容を入力" >お問い合わせ内容</textarea>
+    <input type="checkbox" v-model="isChecked">同意する</input>
+    <button @click="">送信</button>
+    </from>
   </div>
 </template>
-<style scoped></style>
+<style scoped>
+.container{
+  
+}
+</style>
