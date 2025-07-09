66a437a95d1d69617c907b0e8e1839a185d1cd7a
延岡あやか <nobeokaayaka@nobeokaayakanoMacBook-Air.local>
2025-07-09 18:19:13 +0900

    コンポーネント分けの学習

---
+++ vue-practice10/vue-practice10/src/components/BaseButton.vue
@@ -0,0 +1,16 @@
+<!-- BaseButton.vue -->
+<script setup lang="ts">
+type Props = {
+  label: string; // 親から受け取るテキスト（ラベル）
+};
+const props = defineProps<Props>(); // 親からpropsを受け取る
+const emit = defineEmits(["click"]); // 親へイベントを伝える
+</script>
+
+<template>
+  <button @click="$emit('click')">
+    <!-- 親にclickイベントを渡す -->
+    {{ props.label }}
+    <!-- 親からもらったテキストを表示 -->
+  </button>
+</template>
---
+++ vue-practice10/vue-practice10/src/components/BaseInput.vue
@@ -0,0 +1,21 @@
+<!-- BaseInput.vue -->
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+
+type Props = {
+  modelValue: string;
+};
+
+const props = defineProps<Props>();
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+}>();
+</script>
+
+<template>
+  <input
+    type="text"
+    :value="props.modelValue"
+    @input="(e: Event) => emit('update:modelValue', (e.target as HTMLInputElement).value)"
+  />
+</template>
