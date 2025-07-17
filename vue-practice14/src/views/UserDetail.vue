1687d70b8811da0f54fa32f7b8a4da53db84f420
延岡あやか <nobeokaayaka@nobeokaayakanoMacBook-Air.local>
2025-07-17 16:37:07 +0900

    コミット

---
+++ vue-practice14/src/views/UserDetail.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const userId = computed(() => "ユーザーID");
+function goBack(): void {}
+</script>
+
+<template>
+  <div>
+    <h3>ユーザー情報</h3>
+    <p>ユーザーID: {{ userId }}</p>
+    <button @click="goBack">ユーザーリストへ戻る</button>
+  </div>
+</template>
